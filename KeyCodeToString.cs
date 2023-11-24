de4e6c6deb1a2609526bf10075e1a21a6cac0c00
Blauen <85344725+Blauenn@users.noreply.github.com>
2023-11-25 01:39:58 +0700

    Update KeyCodeToString.cs

--- KeyCodeToString.cs
+++ KeyCodeToString.cs
@@ -1,26 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyCodeToString : MonoBehaviour
 {
-    public class KeyCodeToString : MonoBehaviour
-    {
-        public string keycodeString;
+    public Button targetButton; // Reference to the UI button
+    private bool isWaitingForKey = false;
 
-        void Update()
+    void Start()
+    {
+        if (targetButton != null)
         {
-            if (Input.anyKeyDown)
+            targetButton.onClick.AddListener(StartKeyDetection);
+        }
+    }
+
+    void Update()
+    {
+        if (isWaitingForKey && Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
             {
-                foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+                if (Input.GetKeyDown(keyCode))
                 {
-                    if (Input.GetKeyDown(keyCode))
+                    string keyName = Enum.GetName(typeof(KeyCode), keyCode);
+                    Debug.Log("Pressed: " + keyName);
+
+                    // Display the keyName on the button text
+                    if (targetButton != null)
                     {
-                        string keyName = Enum.GetName(typeof(KeyCode), keyCode);
-                        Debug.Log("Pressed: " + keyName);
+                        targetButton.GetComponentInChildren<Text>().text = "Key: " + keyName;
                     }
+
+                    isWaitingForKey = false;
+                    break;
                 }
             }
+        }
+    }
+
+    void StartKeyDetection()
+    {
+        // Set a flag to indicate that we are waiting for a key press
+        isWaitingForKey = true;
+
+        // Clear the button text while waiting for input
+        if (targetButton != null)
+        {
+            targetButton.GetComponentInChildren<Text>().text = "Press a key...";
         }
     }
 }
